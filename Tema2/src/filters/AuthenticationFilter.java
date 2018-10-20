0a1e2d7a728e2ace59c0ab74966af4e299048425
Bogdan RUSU <brusu@ias.bitdefender.biz>
2018-10-20 15:25:50 -0700

    filters package + filter that ensure authentication

---
+++ Tema2/src/filters/AuthenticationFilter.java
@@ -0,0 +1,41 @@
+package filters;
+
+import model.Record;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@WebFilter(filterName = "Authentication")
+public class AuthenticationFilter implements Filter {
+
+    private List<String> excludedUrls;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        this.excludedUrls = Arrays.asList(filterConfig.getInitParameter("excludedUrls").split(", *"));
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest)servletRequest;
+        HttpServletResponse httpRes = (HttpServletResponse)servletResponse;
+
+        // check if url need user to be authenticated
+        if (!this.excludedUrls.contains(httpReq.getRequestURI())){
+            Record record = (Record) httpReq.getSession().getAttribute("user");
+            if (record == null) {
+                // user not authenticated; redirect to sign in page
+                httpRes.sendRedirect("/signin");
+                return;
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
