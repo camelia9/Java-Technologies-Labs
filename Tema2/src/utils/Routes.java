ca93cc02805ac2ed392f35c813a395c84d029055
Bogdan RUSU <brusu@ias.bitdefender.biz>
2018-10-22 17:33:24 -0700

    change route for sign up

--- Tema2/src/utils/Routes.java
+++ Tema2/src/utils/Routes.java
@@ -3,6 +3,6 @@ package utils;
 public class Routes {
 
     public static final String SIGN_IN_ROUTE = "/signin";
-    public static final String SIGN_UP_ROUTE = "/signup"; // aka FORM PAGE
+    public static final String SIGN_UP_ROUTE = "/record"; // aka FORM PAGE
     public static final String USER_HOME_PAGE = "/";
 }
