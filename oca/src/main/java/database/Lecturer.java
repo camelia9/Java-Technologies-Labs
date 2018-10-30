9ffd8f510b9f852fb33817674f9b933b03242e9c
Bogdan RUSU <brusu@ias.bitdefender.biz>
2018-10-29 22:51:16 -0800

    classes with table schema

---
+++ oca/src/main/java/database/Lecturer.java
@@ -0,0 +1,9 @@
+package database;
+
+public class Lecturer {
+
+    public static final String TABLE_NAME = "lecturers";
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String EMAIL = "email";
+}
