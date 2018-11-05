40116418ae8f8978335fbdf193ca8fb1325a1ced
bogdan2442 <rusubogdan77@gmail.com>
2018-11-04 21:19:53 -0800

    implementing listener for sessions

---
+++ oca/src/main/java/listener/SessionCounterListener.java
@@ -0,0 +1,30 @@
+package listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SessionCounterListener implements HttpSessionListener {
+
+    private static final AtomicInteger counter;
+
+    static {
+        counter = new AtomicInteger(0);
+    }
+
+    public synchronized static int getNumberOfSessions(){
+        return counter.get();
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+        counter.incrementAndGet();
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        counter.decrementAndGet();
+    }
+
+
+}
