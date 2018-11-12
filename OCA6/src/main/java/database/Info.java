49c09ec5b98bf3c3e69e77966641398fa67412c7
bogdan2442 <rusubogdan77@gmail.com>
2018-11-12 22:15:26 +0200

    add class with schema for table info

---
+++ OCA6/src/main/java/database/Info.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+/**
+ *
+ * @author brusu
+ */
+public class Info {
+    
+    public final static String TABLE_NAME = "INFO";
+    public final static String ID = "id";
+    public final static String REQUEST_TIME = "request_time";
+    public final static String REMOTE_ADDR = "remote_addr";
+    public final static String REQUEST_PARAMS = "request_params";
+}
