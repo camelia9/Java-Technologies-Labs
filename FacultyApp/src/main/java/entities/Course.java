63d5a4bc4fb554093f5e4a53f7b4fd87f8ad1464
bogdan2442 <rusubogdan77@gmail.com>
2018-11-24 17:55:40 +0200

    add entities lecturer, couse and package with relationships between them

---
+++ FacultyApp/src/main/java/entities/Course.java
@@ -0,0 +1,137 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author brusu
+ */
+@Entity
+public class Course implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="course_id")
+	private Long id;
+	@Column
+	private String abbreviation;
+	@Column
+	private String name;
+	@Column(name="study_year")
+	private int yearStudy;
+	@Column
+	private int semester;
+	@Column(name="credits_number")
+	private int numberOfCredits;
+	@Column(name="course_url")
+	private String courseURL;
+	@ManyToOne
+	private Lecturer lecturer;
+	@ManyToOne
+	@Column(name="package")
+	private CoursePackage coursePackage;
+
+	public String getAbbreviation() {
+		return abbreviation;
+	}
+
+	public void setAbbreviation(String abbreviation) {
+		this.abbreviation = abbreviation;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYearStudy() {
+		return yearStudy;
+	}
+
+	public void setYearStudy(int yearStudy) {
+		this.yearStudy = yearStudy;
+	}
+
+	public int getSemester() {
+		return semester;
+	}
+
+	public void setSemester(int semester) {
+		this.semester = semester;
+	}
+
+	public int getNumberOfCredits() {
+		return numberOfCredits;
+	}
+
+	public void setNumberOfCredits(int numberOfCredits) {
+		this.numberOfCredits = numberOfCredits;
+	}
+
+	public String getCourseURL() {
+		return courseURL;
+	}
+
+	public void setCourseURL(String courseURL) {
+		this.courseURL = courseURL;
+	}
+
+	public Lecturer getLecturer() {
+		return lecturer;
+	}
+
+	public void setLecturer(Lecturer lecturer) {
+		this.lecturer = lecturer;
+	}
+
+	public CoursePackage getCoursePackage() {
+		return coursePackage;
+	}
+
+	public void setCoursePackage(CoursePackage coursePackage) {
+		this.coursePackage = coursePackage;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (id != null ? id.hashCode() : 0);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		// TODO: Warning - this method won't work in the case the id fields are not set
+		if (!(object instanceof Course)) {
+			return false;
+		}
+		Course other = (Course) object;
+		if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "entities.Course[ id=" + id + " ]";
+	}
+}
---
+++ FacultyApp/src/main/java/entities/CoursePackage.java
@@ -0,0 +1,98 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author brusu
+ */
+@Entity
+public class CoursePackage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="package_id")
+	private Long id;
+	@Column(name="package_name")
+	private String name;
+	@Column(name="package_year")
+	private int year;
+	@Column(name="package_semester")
+	private int semester;
+	
+	@OneToMany(targetEntity=Course.class)
+	private List<Course> courses;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getSemester() {
+		return semester;
+	}
+
+	public void setSemester(int semester) {
+		this.semester = semester;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (id != null ? id.hashCode() : 0);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		// TODO: Warning - this method won't work in the case the id fields are not set
+		if (!(object instanceof CoursePackage)) {
+			return false;
+		}
+		CoursePackage other = (CoursePackage) object;
+		if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "entities.CoursePackage[ id=" + id + " ]";
+	}
+}
