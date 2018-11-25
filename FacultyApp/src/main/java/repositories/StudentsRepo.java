b5e417b62922d8a8b2ab030b957a4beabb93e384
bogdan2442 <rusubogdan77@gmail.com>
2018-11-25 22:38:19 +0200

    add repository for table 'students'

---
+++ FacultyApp/src/main/java/repositories/StudentsRepo.java
@@ -0,0 +1,40 @@
+package repositories;
+
+import entities.Student;
+import java.util.List;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author brusu
+ */
+public class StudentsRepo extends GenericRepo<Student> {
+    
+    private Query selectAll;
+    
+    public StudentsRepo() {
+        super(Student.class);
+    }
+    
+    public List<Student> getStudents(){
+        if(selectAll == null)
+            selectAll = em.createQuery("SELECT s FROM Student s");
+        return selectAll.getResultList();
+    }
+    
+    public Student getStudent(long studentId){
+        return getEntity(studentId);
+    }
+    
+    public void insertStudent(Student student){
+        insertEntity(student);
+    }
+    
+    public void updateStudent(Student student){
+        updateEntity(student);
+    }
+    
+    public void deleteStudent(long studentId){
+        deleteEntity(studentId);
+    }
+}
