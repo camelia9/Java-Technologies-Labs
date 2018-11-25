e03ea0a55088514f3d460166b6c9b6f725e25f8e
bogdan2442 <rusubogdan77@gmail.com>
2018-11-25 13:48:59 +0200

    add repo for 'packages' table

---
+++ FacultyApp/src/main/java/repositories/PackagesRepo.java
@@ -0,0 +1,41 @@
+package repositories;
+
+import entities.CoursePackage;
+import java.util.List;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author brusu
+ */
+public class PackagesRepo extends GenericRepo<CoursePackage> {
+    
+    Query selectAll = null;
+    
+   public PackagesRepo(){
+       super(CoursePackage.class);
+   }
+   
+   public List<CoursePackage> getPackages(){
+       if(selectAll == null){
+           selectAll = em.createQuery("SELECT p FROM CoursePackage p");
+       }
+       return selectAll.getResultList();
+   }
+   
+   public CoursePackage getPackage(long packageId){
+       return getEntity(packageId);
+   }
+   
+   public void insertPackage(CoursePackage newPackage){
+       insertEntity(newPackage);
+   }
+   
+   public void updatePackage(CoursePackage updatedPackage){
+       updateEntity(updatedPackage);
+   }
+   
+   public void deletePackage(long packageId){
+       deleteEntity(packageId);
+   }
+}
