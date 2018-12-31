f07810250d097b1dea056bb39c4ae980ec8ac9dc
Camelia-Maria Milut <milut@amazon.com>
2018-12-31 14:01:48 +0200

    Add entities classes

--- CDI/src/main/java/entities/Users.java
+++ CDI/src/main/java/entities/Users.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,6 +9,6 @@ package entities;
  *
  * @author milut
  */
-public class Documents {
+public class Users {
     
 }
