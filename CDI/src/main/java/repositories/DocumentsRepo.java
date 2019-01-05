15065c226282229f88bd5303465346a6c0de8748
bogdan2442 <rusubogdan77@gmail.com>
2019-01-05 22:11:15 +0200

    start implement docs repo

--- CDI/src/main/java/repositories/DocumentsRepo.java
+++ CDI/src/main/java/repositories/DocumentsRepo.java
@@ -5,10 +5,23 @@
  */
 package repositories;
 
+import entities.Documents;
+import entities.Users;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  *
  * @author milut
  */
+@Stateless
 public class DocumentsRepo {
     
+    @PersistenceContext(name="persistenceUnit") 
+    private EntityManager entityManager;
+    
+    public void insertDocument(Documents document){
+        entityManager.persist(document);
+    }
 }
