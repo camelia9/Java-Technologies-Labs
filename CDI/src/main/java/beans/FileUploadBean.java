8b8cdc5765354d9c96be180cf91d6e69ab8ca551
bogdan2442 <rusubogdan77@gmail.com>
2019-01-05 22:10:49 +0200

    implement upload bean without db save

--- CDI/src/main/java/beans/FileUploadBean.java
+++ CDI/src/main/java/beans/FileUploadBean.java
@@ -5,9 +5,13 @@
  */
 package beans;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.servlet.http.Part;
+import org.primefaces.model.UploadedFile;
 /**
  *
  * @author milut
@@ -17,5 +21,33 @@ import javax.inject.Named;
 @SessionScoped
 public class FileUploadBean implements Serializable{
     
+    private UploadedFile file;
+    
+    public void setFile(UploadedFile file){
+        this.file = file;
+    }
+    
+    public UploadedFile getFile(){
+        return this.file;
+    }
+    
+    public void save(){
+        String fileName = file.getFileName();
+        int fileSize = (int)file.getSize();
+        
+        System.out.println("[DEBUG] FILE SIZE " + fileSize);
+        System.out.println("[DEBUG] FILE NAME is " + fileName);
+        try (InputStream input = file.getInputstream()) {
+            byte fileBuffer[] = new byte[fileSize];
+            int bytesRead = input.read(fileBuffer);
+            while(bytesRead < fileSize){
+                bytesRead += input.read(fileBuffer, bytesRead, fileSize - bytesRead);
+            }
+            System.out.println("[DEBUG] File content \n" + new String(fileBuffer));
+        }
+        catch (IOException e) {
+            // maybe show in interface
+        }
+    }
 }
 
